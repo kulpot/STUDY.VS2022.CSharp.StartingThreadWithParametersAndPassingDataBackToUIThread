a2fcd9fc6563b4949f75b4a5f7c60fa6b7dcb83e
Kulpot <kulpotwahu@gmail.com>
2023-05-31 06:05:57 +0800

    Add project files.

---
+++ StartingThreadWithParametersAndPassingDataBackToUIThread/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace StartingThreadWithParametersAndPassingDataBackToUIThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(349, 328);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(116, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(453, 310);
+            this.textBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 363);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ StartingThreadWithParametersAndPassingDataBackToUIThread/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+//ref link:https://www.youtube.com/watch?v=fxylJGbEsmE&list=PLAIBPfq19p2EJ6JY0f5DyQfybwBGVglcR&index=77
+//Starting a thread with parameters and passing data back to the UI Thread
+
+namespace StartingThreadWithParametersAndPassingDataBackToUIThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    ParameterizedThreadStart start = new ParameterizedThreadStart(LoadFile);
+                    Thread thread = new Thread(start);
+                    thread.Start(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void LoadFile(object fileName)
+        {
+            try
+            {
+                string content = File.ReadAllText(fileName.ToString());
+                SetTextBoxTextCallBack(content); //textBox.Invoke(new MethodInvoker(delegate { textBox.Text = content; })); // this is shorter line ....replace this line and remove its method
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Set textbox text in a thread safe manner using method invoker
+        /// </summary>
+        private void SetTextBoxTextCallBack(string text)
+        {
+            MethodInvoker invoker = new MethodInvoker(delegate { textBox.Text = text; });
+            textBox.Invoke(invoker);
+
+            //textBox.Invoke(new MethodInvoker(delegate { textBox.Text = text; }));
+        }
+    }
+}
